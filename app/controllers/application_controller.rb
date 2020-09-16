faa70b81d0f3c5879a138b4889d84e978e7f1ef9
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-09-15 22:44:52 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,57 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+#READ ALL
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+#CREATE GET
+  get '/articles/new' do
+
+    erb :new
+  end
+
+#CREATE POST
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{Article.last.id}"
+  end
+
+#READ POST SINGLE
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+#UPDATE GET
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+#UPDATE PATCH
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    erb :show
+  end
+
+
+#DELETE
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    
+    @article.destroy
+    @articles = Article.all
+
+    redirect to "/articles"
+
+    erb :index
+  end
+
+
 end

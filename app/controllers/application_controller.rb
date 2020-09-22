d317841b4d3ed52f5de4782f6efe2957cb09bbc9
kowalix-pl <kowalix007@gmail.com>
2020-09-22 20:02:53 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,52 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enable :sessions
+    # set :session_secret, 'secret'
   end
 
   get '/' do
+   
+     erb :index
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/new' do
+    
+    erb :new
+  end 
+
+  get '/articles/:id' do
+     @article = Article.find(params[:id])
+    erb :show
+  end 
+  
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+  end 
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+  
+    redirect "/articles/#{@article.id}"
+  end 
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end 
+  
+  patch '/articles/:id' do
+   @article = Article.find(params[:id])
+   @article.update(title: params[:title], content: params[:content])
+   redirect "/articles/#{@article.id}"
+   
+   
+  end 
+
 end
